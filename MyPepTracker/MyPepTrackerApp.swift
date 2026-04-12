a244b916def4037059df0fc77a2310f803eeb762
Greg Roy <greg.roy@metricaid.com>
2026-04-12 17:59:26 -0400

    feat: add notification permission request and category registration on app launch

--- MyPepTracker/MyPepTrackerApp.swift
+++ MyPepTracker/MyPepTrackerApp.swift
@@ -6,7 +6,18 @@ struct MyPepTrackerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .task {
+                    await setupNotifications()
+                }
         }
         .modelContainer(for: [Peptide.self, Vial.self, DoseEntry.self])
+    }
+
+    private func setupNotifications() async {
+        let manager = NotificationManager.shared
+        let granted = await manager.requestPermission()
+        if granted {
+            manager.registerCategories()
+        }
     }
 }
